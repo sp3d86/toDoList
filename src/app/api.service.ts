bbc5a82a7c7c39d4a588f1b83f84faaecd5e9747
sp3d86 <sp3d86@gmail.com>
2019-12-05 23:44:37 +0300

    fix for npm start and http get

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'environments/environment';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient  } from '@angular/common/http';
 import { Todo } from './todo';
 import { Observable } from 'rxjs/Observable';
-import { Subject } from 'rxjs';
-import 'rxjs/add/operator/map';
+import { map } from 'rxjs/operators';
+//import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/observable/throw';
-import { AWS } from '@aws-amplify/core';
+
 
 const API_URL = environment.apiUrl;
 
@@ -15,49 +15,49 @@ const API_URL = environment.apiUrl;
 @Injectable()
 export class ApiService {
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient ) {}
 
-  public getAllTodos(): Observable<Todo[]> {
+  public getAllTodos(): Observable<any> {
     return this.http
       .get(API_URL + '/todos')
-      .map(response => {
-        const todos = response['todos'];
+      .pipe(map((response : any)=> {
+        const todos = response; 
         return todos.map((todo) => new Todo(todo));
-      })
+      }))
       .catch(this.handleError);
   }
 
   public createTodo(todo: Todo): Observable<Todo> {
     return this.http
       .post(API_URL + '/todos', todo)
-      .map(response => {
-        return new Todo(response['todos']);
-      })
+      .pipe(map(response => {
+        return new Todo(response);
+      }))
       .catch(this.handleError);
   }
 
   public getTodoById(todoId: number): Observable<Todo> {
     return this.http
       .get(API_URL + '/todos/' + todoId)
-      .map(response => {
-        return new Todo(response['todos']);
-      })
+      .pipe(map(response => {
+        return new Todo(response);
+      }))
       .catch(this.handleError);
   }
 
   public updateTodo(todo: Todo): Observable<Todo> {
     return this.http
       .put(API_URL + '/todos/' + todo.id, todo)
-      .map(response => {
-        return new Todo(response['todos']);
-      })
+      .pipe(map(response => {
+        return new Todo(response);
+      }))
       .catch(this.handleError);
   }
 
   public deleteTodoById(todoId: number): Observable<null> {
     return this.http
       .delete(API_URL + '/todos/' + todoId)
-      .map(response => null)
+      .pipe(map(response => null))
       .catch(this.handleError);
   }
 
